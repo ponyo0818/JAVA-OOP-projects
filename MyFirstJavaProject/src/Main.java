341bb1079f8bf40239cff0589da8fbf00b128b2b
Yuan Fang <43736431+ponyo0818@users.noreply.github.com>
2019-04-24 18:19:42 -0400

    first commision

---
+++ MyFirstJavaProject/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * The main method:
+ *
+ * public: Means you can run this method from anywhere in your Java program (we will talk more about public and private methods later
+ * static: Means it doesn't need an object to run, which is why the computer starts with this method before even creating any objects (we will also talk more about static methods later on)
+ * void: Means the main method doesn't return anything, it just runs when the program starts, and once it's done the program terminates
+ * main: Is the name of the method
+ * String [] args : Is the input parameter (array of strings) which we will cover how to use it later in this lesson as well!
+*/
+public class Main {
+    public static void main(String [] args){
+        System.out.println("Hello world!");
+    }
+}
+ 
